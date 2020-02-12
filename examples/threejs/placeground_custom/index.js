5807819c2998ec2a5b9c022d725977ab9f681269
fujimotonaoaki <naokiring@gmail.com>
2020-02-13 00:43:00 +0900

    add customThreejsPipelineModule

--- examples/threejs/placeground_custom/index.js
+++ examples/threejs/placeground_custom/index.js
@@ -1,5 +1,7 @@
 // Copyright (c) 2018 8th Wall, Inc.
 
+const myThreejsModule = customThreejsPipelineModule()
+
 // Returns a pipeline module that initializes the threejs scene when the camera feed starts, and
 // handles subsequent spawning of a glb model whenever the scene is tapped.
 const placegroundScenePipelineModule = () => {
@@ -46,7 +48,7 @@ const placegroundScenePipelineModule = () => {
     model.scene.rotation.set(0.0, yDegrees, 0.0)
     model.scene.position.set(pointX, 0.0, pointZ)
     model.scene.scale.set(scale.x, scale.y, scale.z)
-    XR8.Threejs.xrScene().scene.add(model.scene)
+    myThreejsModule.xrScene().scene.add(model.scene)
 
     new TWEEN.Tween(scale)
       .to(endScale, animationMillis)
@@ -79,7 +81,7 @@ const placegroundScenePipelineModule = () => {
     }
 
     // If the canvas is tapped with one finger and hits the "surface", spawn an object.
-    const {scene, camera} = XR8.Threejs.xrScene()
+    const {scene, camera} = myThreejsModule.xrScene()
 
     // calculate tap position in normalized device coordinates (-1 to +1) for both components.
     tapPosition.x = (e.touches[0].clientX / window.innerWidth) * 2 - 1
@@ -104,7 +106,7 @@ const placegroundScenePipelineModule = () => {
     // XR8.Threejs scene to be ready before we can access it to add content. It was created in
     // XR8.Threejs.pipelineModule()'s onStart method.
     onStart: ({canvas, canvasWidth, canvasHeight}) => {
-      const {scene, camera} = XR8.Threejs.xrScene()  // Get the 3js sceen from xr3js.
+      const {scene, camera} = myThreejsModule.xrScene()  // Get the 3js sceen from xr3js.
 
       initXrScene({ scene, camera }) // Add objects to the scene and set starting camera position.
 
@@ -130,7 +132,7 @@ const onxrloaded = () => {
   XR8.addCameraPipelineModules([  // Add camera pipeline modules.
     // Existing pipeline modules.
     XR8.GlTextureRenderer.pipelineModule(),      // Draws the camera feed.
-    XR8.Threejs.pipelineModule(),                // Creates a ThreeJS AR Scene.
+    myThreejsModule,                // Creates a ThreeJS AR Scene.
     XR8.XrController.pipelineModule(),           // Enables SLAM tracking.
     XRExtras.AlmostThere.pipelineModule(),       // Detects unsupported browsers and gives hints.
     XRExtras.FullWindowCanvas.pipelineModule(),  // Modifies the canvas to fill the window.
